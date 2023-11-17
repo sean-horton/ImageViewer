d083d5d5a415f6613559ef7a2ca60d2f79129ed6
Sean Horton <3359434+sean-horton@users.noreply.github.com>
2023-11-16 21:21:39 -0500

    add app icons

--- macosIcns.sh
+++ macosIcns.sh
@@ -1,4 +1,12 @@
+
+# WINDOWS needs a 24 bit ico compressed
+# Add all the images in gimp and then export as ico
+
+# MACOS: Run this script to make macos icons
+# note that macos icns file should have extra padding, so it's actually
+# a different image than the other icons
 mkdir app-icon.iconset
+
 sips -z 16 16     src/main/resources/image/branding/app-icon.png --out app-icon.iconset/icon_16x16.png
 sips -z 32 32     src/main/resources/image/branding/app-icon.png --out app-icon.iconset/icon_16x16@2x.png
 sips -z 32 32     src/main/resources/image/branding/app-icon.png --out app-icon.iconset/icon_32x32.png
@@ -9,4 +17,7 @@ sips -z 256 256   src/main/resources/image/branding/app-icon.png --out app-icon.
 sips -z 512 512   src/main/resources/image/branding/app-icon.png --out app-icon.iconset/icon_256x256@2x.png
 sips -z 512 512   src/main/resources/image/branding/app-icon.png --out app-icon.iconset/icon_512x512.png
 iconutil -c icns app-icon.iconset
+
+mv app-icon.icns src/main/resources/image/branding
+
 rm -R app-icon.iconset
